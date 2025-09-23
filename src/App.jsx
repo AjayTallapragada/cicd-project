68d6006e80745416c79495d0262f7602483c0aac
Ajay Tallapragada <ajaytallapragada2207@gmail.com>
2025-09-23 09:41:24 +0530

    first version

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Stock from './pages/Stock';
+import AboutUs from './pages/AboutUs';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/stock" element={<Stock />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about us page for the inventory management system.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
